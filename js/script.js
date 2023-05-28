5b44370200ec723819965ed0b3e3f1facabfd3c0
han chang hee <hupo119@naver.com>
2023-05-28 20:07:51 +0900

    07_마이캐논 + 08_개인정보수정 UI완료

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/* 01.메인페이지 */
+
+/* 02.로그인 */
+
+/* 03.통합회원전환 */
+
+/* 04.통합회원가입페이지 */
+
+/* 05.통합회원가입(개인) */
+
+/* 06.통합회원가입(사업자) */
+
 /* 07.마이캐논 */
 function addContents(){
   var tableTag = document.querySelector('.coupon_table');
@@ -10,3 +22,31 @@ function removeContents(){
   var tableTag = document.querySelector('.coupon_table tr');
   tableTag.remove();
 }
+
+
+
+/* 08.개인정보수정 */
+
+
+/* 09.1:1문의 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
